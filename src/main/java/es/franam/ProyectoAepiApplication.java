eae91fe7e10507a47dfe19c6cd9bd50ac753c952
Fran <franciscojose.aunon@gmail.com>
2020-06-11 08:23:46 +0200

    Primer commit

---
+++ src/main/java/es/franam/ProyectoAepiApplication.java
@@ -0,0 +1,13 @@
+package es.franam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoAepiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoAepiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/franam/ProyectoAepiApplicationTests.java
@@ -0,0 +1,13 @@
+package es.franam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoAepiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
