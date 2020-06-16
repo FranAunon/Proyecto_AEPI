b48217e79b13446b4cc630050d637102b0cdfec5
Fran <franciscojose.aunon@gmail.com>
2020-06-16 18:44:15 +0200

    Agregando vista detalles de libro, menu y footer con fragments

--- src/main/java/es/franam/controller/BookController.java
+++ src/main/java/es/franam/controller/BookController.java
@@ -9,6 +9,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -35,6 +36,15 @@ public class BookController {
 		List<Book> books = bookService.buscarTodas();
 		model.addAttribute("books", books);
 		return "home";
+	}
+	
+	@GetMapping("/books/view/{id}")
+	public String verDetalle(@PathVariable("id") int id, Model model) {		
+		Book book = bookService.buscarPorId(id);	
+		System.out.println("Book: " + book);
+		model.addAttribute("book", book);
+			
+		return "detalle";
 	}
 	
 	@GetMapping({"/book", "/book/list"})
