97877000c4b97d04c59216e8fb5126ad3dbbe912
Fran <franciscojose.aunon@gmail.com>
2020-06-18 08:13:06 +0200

    Retocar templates

--- src/main/java/es/franam/repository/EditorialRepository.java
+++ src/main/java/es/franam/repository/EditorialRepository.java
@@ -1,10 +1,11 @@
 package es.franam.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import es.franam.domain.Editorial;
 
-
+@Repository
 public interface EditorialRepository extends JpaRepository<Editorial, Integer> {
 	
 }
--- src/main/java/es/franam/repository/PerfilesRepository.java
+++ src/main/java/es/franam/repository/PerfilesRepository.java
@@ -1,9 +1,11 @@
 package es.franam.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import es.franam.domain.Perfil;
 
+@Repository
 public interface PerfilesRepository extends JpaRepository<Perfil, Integer> {
 
 }
